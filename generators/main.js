de2754cefe6ac793d2dff1d1dc04cd70b0b2ccdf
vireon <3401681691@qq.com>
2019-09-15 14:45:27 +0800

    test

---
+++ generators/main.js
@@ -0,0 +1,15 @@
+class Cat {
+  constructor(prefix, suffix) {
+    this.prefix = prefix;
+    this.suffix = suffix;
+  }
+
+  Name() {
+    return `${this.prefix}${this.suffix}`.toUpperCase()
+  }
+}
+
+let generatedCat = new Cat('needle','whisker');
+
+const displayName = document.querySelector('#displayName')
+displayName.textContent = generatedCat.Name;
